f3d192b25f725046535eeaee0890ae1e8607cfb1
Syed M. Sawaid <34231494+SyedMSawaid@users.noreply.github.com>
2024-07-05 20:11:23 +0500

    fix: renderer

--- lib/inertia_flow/initializers.rb
+++ lib/inertia_flow/initializers.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
+require "jbuilder"
 require_relative "renderer"
 
 Mime::Type.register_alias "application/json", :inertia
 
-ActionController::Renderers.add :inertia, InertiaFlow::Renderer
+ActionController::Renderers.add :inertia do |component, options|
+  controller = self
+
+  InertiaFlow::Renderer.new(component, options, controller, request, response).render
+end
--- lib/inertia_flow/renderer.rb
+++ lib/inertia_flow/renderer.rb
@@ -1,16 +1,28 @@
 # frozen_string_literal: true
 
+require "jbuilder/jbuilder_template"
+
 module InertiaFlow
   class Renderer
-    def call(component, options)
+    attr_accessor :component, :options, :controller, :request, :response
+
+    def initialize(component, options, controller, request, response)
+      @component = component
+      @options = options
+      @controller = controller
+      @request = request
+      @response = response
+    end
+
+    def render
       props = jbuilder_attributes
 
       InertiaRails::Renderer.new(
         component,
-        self,
+        controller,
         request,
         response,
-        method(:render),
+        render_method,
         props: props,
         view_data: options[:view_data]
       ).render
@@ -22,13 +34,33 @@ module InertiaFlow
       jbuilder = JbuilderTemplate.new(view_context)
 
       # Render the Jbuilder template into the Jbuilder object
-      view_context.render(template: "#{controller_path}/#{action_name}",
+      view_context.render(template: template,
                           formats: [:inertia],
                           handlers: [:jbuilder],
                           locals: { json: jbuilder })
 
       # Get the Ruby hash from the Jbuilder object
       jbuilder.attributes!
+    end
+
+    def template
+      "#{controller_path}/#{action_name}"
+    end
+
+    def render_method
+      controller.method(:render)
+    end
+
+    def view_context
+      controller.view_context
+    end
+
+    def controller_path
+      controller.controller_path
+    end
+
+    def action_name
+      controller.action_name
     end
   end
 end
